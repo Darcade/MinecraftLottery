b15ccb3e1f098f191c0c16181c38b2715e4721dd
Darcade <darcade@darcade.de>
2014-03-10 13:24:04 +0100

    Small fixes

--- src/de/darcade/minecraftlottery/CommandExecutorClass.java
+++ src/de/darcade/minecraftlottery/CommandExecutorClass.java
@@ -1,6 +1,5 @@
 package de.darcade.minecraftlottery;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
